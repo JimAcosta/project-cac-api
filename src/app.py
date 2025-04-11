4c2da6e0e96e7dc381333d3d1912bee347ee7154
Jim Yang <jimmyhorseman95@gmail.com>
2025-04-10 23:24:53 -0300

    prueba de error

--- src/app.py
+++ src/app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 CORS(app)
 
 def page_not_found(error):
-    return '<h1>Not found page</h1>',404
+    return '<h1>No funciona mostro</h1>',404
 
 if __name__== '__main__':
 
     app.config.from_object(config['development'])
     app.register_blueprint(Alumno.main, url_prefix = '/api/alumnos')
-    app.register_error_handler(408,page_not_found)
+    app.register_error_handler(404,page_not_found)
     app.run()
