8665ded6d29430640652a024d86773030b602ead
Jim Yang <jimmyhorseman95@gmail.com>
2025-04-14 17:15:32 -0300

    se configura puerto de render

--- src/app.py
+++ src/app.py
@@ -8,16 +8,18 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+app.config.from_object(config['development'])
+app.register_blueprint(Alumno.main, url_prefix='/')
+app.register_blueprint(test_db)
+
+# ✅ Definir antes de registrar
 def page_not_found(error):
     print("Entró al error 404")  # DEBUG
-    return '<h1>No funciona mostro</h1>',404
+    return '<h1>No funciona mostro</h1>', 404
+
+app.register_error_handler(404, page_not_found)
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
-    app.register_blueprint(Alumno.main, url_prefix='/')
-    app.register_blueprint(test_db)
-    app.register_error_handler(404, page_not_found)
-
-    # ⚠️ SOLO este app.run()
-    port = int(os.environ.get("PORT", 5000))  # Render lo pone como PORT
+    # ✅ Render asigna automáticamente el puerto al env "PORT"
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
