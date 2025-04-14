fbe415efcd3242917ef23b3a1091bc146c6c3963
Jim Yang <jimmyhorseman95@gmail.com>
2025-04-14 16:43:14 -0300

    actualizo .env

--- src/database/db.py
+++ src/database/db.py
@@ -18,6 +18,13 @@ def get_db():
         try:
             g.db = psycopg2.connect(**DATABASE_CONFIG)
         except Exception as e:
+            print({
+                'host': os.getenv('DB_HOST'),
+                'user': os.getenv('DB_USERNAME'),
+                'password': os.getenv('DB_PASSWORD'),
+                'database': os.getenv('DB_NAME'),
+                'port': int(os.getenv('DB_PORT', 5432))  # Puerto de PostgreSQL
+            })
             print("❌ Error al conectar a la base de datos:", e)
             raise
     return g.db
