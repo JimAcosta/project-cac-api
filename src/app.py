1117676310ae197e956f84d7790d77e237a448bd
JimAcosta <jimmyhorseman95@gmail.com>
2025-04-14 20:25:37 -0300

    Limpieza final y preparacion para produccion

--- src/app.py
+++ src/app.py
@@ -12,14 +12,13 @@ app.config.from_object(config['development'])
 app.register_blueprint(Alumno.main, url_prefix='/')
 app.register_blueprint(test_db)
 
-# ✅ Definir antes de registrar
+
 def page_not_found(error):
-    print("Entró al error 404")  # DEBUG
-    return '<h1>No funciona mostro</h1>', 404
+    return '<h1>No Se Encontro Url</h1>', 404
 
 app.register_error_handler(404, page_not_found)
 
 if __name__ == '__main__':
-    # ✅ Render asigna automáticamente el puerto al env "PORT"
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- src/routes/Alumno.py
+++ src/routes/Alumno.py
@@ -7,21 +7,11 @@ main = Blueprint('alumno_blueprint',__name__)
 
 @main.route('/')
 def get_alumnos():
-    print("⚡ Entró a la ruta principal /")
     try:
         alumnos = AlumnoModel.get_alumnos()
         return jsonify(alumnos)
     except Exception as ex:
-        print("❌ Ocurrió un error: ", ex)
-        return jsonify({'mensaje pa': str(ex)})
-
-@main.route('/test')
-def test():
-    return 'API online mostro'
-
-@main.route('/ping')
-def ping():
-    return jsonify({'status': 'API funcionando en producción 🔥'})
+        return jsonify({'mesagge pa': str(ex)})
     
 @main.route('/get_alumno/<email>', methods=['GET'])
 def get_alumno(email):
